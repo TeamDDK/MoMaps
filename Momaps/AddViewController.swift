0f58aa674d00a159821a1590209ae2f8a7e404db
Dibba <roydibba@gmail.com>
2021-05-06 01:14:28 -0400

    Added functionality to creates classes on the Parse Database

--- Momaps/AddViewController.swift
+++ Momaps/AddViewController.swift
@@ -6,28 +6,72 @@
 //
 
 import UIKit
+import Parse
 
 class AddViewController: UIViewController {
-    @IBOutlet weak var slider: UISegmentedControl!
-    
+
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
-    
     @IBOutlet weak var descriptionlabel: UILabel!
-    
-    
     @IBOutlet weak var nameTextField: UITextField!
-    
     @IBOutlet weak var addressTextField: UITextField!
-    
     @IBOutlet weak var descriptionTextField: UITextField!
     
-    @IBAction func saveButton(_ sender: Any) {
+    let FaveLocations = PFObject(className: "FaveLocations")
+    let PlanLocations = PFObject(className: "PlanLocations")
+
+    @IBAction func addFavoritesButton(_ sender: Any) {
+        FaveLocations["Name"] = nameTextField.text!
+        FaveLocations["Address"] = addressTextField.text!
+        FaveLocations["Description"] = descriptionTextField.text!
+        FaveLocations["User"] = PFUser.current()
+        
+        FaveLocations.saveInBackground { success, error in
+            if success{
+                print("YAAY")
+            }else{
+                print("Error nigga")
+            }
+        }
     }
+    
+    
+    @IBAction func addPlansButton(_ sender: Any) {
+        PlanLocations["Name"] = nameTextField.text!
+        PlanLocations["Address"] = addressTextField.text!
+        PlanLocations["Description"] = descriptionTextField.text!
+        PlanLocations["User"] = PFUser.current()
+        
+        PlanLocations.saveInBackground { success, error in
+            if success{
+                print("YAAY")
+            }else{
+                print("Error nigga")
+            }
+        }
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        nameTextField.layer.cornerRadius = 10.0
+        nameTextField.layer.borderWidth = 2.0
+        nameTextField.layer.borderColor = UIColor.black.cgColor
+        nameTextField.layer.masksToBounds = true
+        
+        addressTextField.layer.cornerRadius = 10.0
+        addressTextField.layer.borderWidth = 2.0
+        addressTextField.layer.borderColor = UIColor.black.cgColor
+        addressTextField.layer.masksToBounds = true
+        
+        descriptionTextField.layer.cornerRadius = 10.0
+        descriptionTextField.layer.borderWidth = 2.0
+        descriptionTextField.layer.borderColor = UIColor.black.cgColor
+        descriptionTextField.layer.masksToBounds = true
+        descriptionTextField.textAlignment = .left
+        descriptionTextField.contentVerticalAlignment = .top
+
     }
     
 
