cb56b3dfdd3164394c13f832aea3c304e50d0645
dew613 <dewansunnah613@gmail.com>
2021-05-07 11:59:32 -0400

    implement routes to specified location, made it automatically calculate route when tapping an annotation, calculate route function is its own function

--- Momaps/HomeViewController.swift
+++ Momaps/HomeViewController.swift
@@ -10,6 +10,9 @@ import Parse
 import NotificationBannerSwift
 import Lottie
 import Mapbox
+import MapboxCoreNavigation
+import MapboxNavigation
+import MapboxDirections
 
 
 
@@ -22,7 +25,11 @@ extension UIViewController{
         view.endEditing(true)
     }
 }
+
 class HomeViewController: UIViewController, MGLMapViewDelegate {
+    var mapView: NavigationMapView!
+    var routeOptions: NavigationRouteOptions?
+    var route: Route?
     
     @IBAction func logoutButton(_ sender: Any) {
         PFUser.logOut()
@@ -45,11 +52,12 @@ class HomeViewController: UIViewController, MGLMapViewDelegate {
         
         //dark mode style
         let url = URL(string: "mapbox://styles/mapbox/dark-v10")
-        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
+        mapView = NavigationMapView(frame: view.bounds, styleURL: url)
         
         //initial map loads like this
+        //zoom level gets closer as you increase the number
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 40.71, longitude: -74.00), zoomLevel: 9, animated: false)
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 40.74699, longitude: -73.98742), zoomLevel: 12, animated: false)
         view.addSubview(mapView)
         
         /*
@@ -91,10 +99,91 @@ class HomeViewController: UIViewController, MGLMapViewDelegate {
     }
     
     func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
-    let camera = MGLMapCamera(lookingAtCenter: annotation.coordinate, fromDistance: 4500, pitch: 15, heading: 180)
+        /*//tries to find the distance in meters between 2 coordinates
+        let from = CLLocation(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+        let user_pos = mapView.userLocation?.coordinate
+        let to = CLLocation(latitude: user_pos!.latitude, longitude: user_pos!.longitude)
+        let _distance = from.distance(from: to)
+        */
+        //print(_distance)
+        
+        /*   //commenting this out because across distance maybe bugged
+        let camera = MGLMapCamera(lookingAtCenter: annotation.coordinate, acrossDistance: (_distance*2), pitch: 15, heading: 180)
     mapView.fly(to: camera, withDuration: 4,
     peakAltitude: 3000, completionHandler: nil)
+    */
+    
+        let coordinate = annotation.coordinate
+        
+        
+         
+        if let origin = mapView.userLocation?.coordinate {
+        // Calculate the route from the user's location to the set destination
+        calculateRoute(from: origin, to: coordinate)
+        } else {
+        print("Failed to get user location, make sure to allow location access for this application.")
+        }
+        
     }
+    
+    // Calculate route to be used for navigation
+    func calculateRoute(from origin: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D) {
+        // Coordinate accuracy is how close the route must come to the waypoint in order to be considered viable. It is measured in meters. A negative value indicates that the route is viable regardless of how far the route is from the waypoint.
+        let origin = Waypoint(coordinate: origin, coordinateAccuracy: -1, name: "Start")
+        let destination = Waypoint(coordinate: destination, coordinateAccuracy: -1, name: "Finish")
+
+        // Specify that the route is intended for automobiles avoiding traffic
+        let routeOptions = NavigationRouteOptions(waypoints: [origin, destination], profileIdentifier: .automobileAvoidingTraffic)
+
+        // Generate the route object and draw it on the map
+        Directions.shared.calculate(routeOptions) { [weak self] (session, result) in
+            switch result {
+            case .failure(let error):
+                print(error.localizedDescription)
+            case .success(let response):
+                guard let route = response.routes?.first, let strongSelf = self else {
+                    return
+                }
+                
+                strongSelf.route = route
+                strongSelf.routeOptions = routeOptions
+                
+                
+                // Draw the route on the map after creating it
+                strongSelf.drawRoute(route: route)
+                
+                // Show destination waypoint on the map
+                strongSelf.mapView.showWaypoints(on: route)
+            
+            }
+        }
+    }
+    
+    func drawRoute(route: Route) {
+        guard let routeShape = route.shape, routeShape.coordinates.count > 0 else { return }
+        // Convert the route’s coordinates into a polyline
+        var routeCoordinates = routeShape.coordinates
+        let polyline = MGLPolylineFeature(coordinates: &routeCoordinates, count: UInt(routeCoordinates.count))
+
+        // If there's already a route line on the map, reset its shape to the new route
+        if let source = mapView.style?.source(withIdentifier: "route-source") as? MGLShapeSource {
+            source.shape = polyline
+        } else {
+            let source = MGLShapeSource(identifier: "route-source", features: [polyline], options: nil)
+
+            // Customize the route line color and width
+            let lineStyle = MGLLineStyleLayer(identifier: "route-style", source: source)
+            lineStyle.lineColor = NSExpression(forConstantValue: #colorLiteral(red: 0.1897518039, green: 0.3010634184, blue: 0.7994888425, alpha: 1))
+            lineStyle.lineWidth = NSExpression(forConstantValue: 3)
+
+            // Add the source and style layer of the route line to the map
+            mapView.style?.addSource(source)
+            mapView.style?.addLayer(lineStyle)
+        }
+    }
+    
+
+
     
     /*
     // MARK: - Navigation
